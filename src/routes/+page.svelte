7c603a3b5be0fa172506452e1f3f1bbb96a81064
Jess Martin <jessmartin@gmail.com>
2022-10-26 12:33:19 -0400

    Initial SvelteKit app

---
+++ src/routes/+page.svelte
@@ -0,0 +1,15 @@
+<div class="wrapper">
+	<button id="butOpenFile">Open File</button>
+
+	<div class="board">
+		<div id="todoDrop" class="drop-zone">
+			<h1>Todo</h1>
+			<ul id="todoList" class="lane" />
+		</div>
+
+		<div id="doneDrop" class="drop-zone">
+			<h1>Done</h1>
+			<ul id="doneList" class="lane" />
+		</div>
+	</div>
+</div>
